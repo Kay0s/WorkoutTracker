a5e49a8ed2afff3deddb6aefd4b1a8ea86768d9c
Kay0s <hamilton.kristina@gmail.com>
2021-02-04 16:37:29 -0600

    updated app on workout.js and api.js

--- routes/api.js
+++ routes/api.js
@@ -12,14 +12,44 @@ module.exports = (app) => {
   });
 
   // addExcercise(data)
-  app.post("/api/workouts", async ({ body }, res) => {
+  app.put("/api/workouts", async ({ body }, res) => {
     db.Workout.create(body).then(({ _id }) =>
-      db.Workout.findOneAndUpdate({}, { $push: { name: _id } }, { new: true })
+      db.Workout.findOneAndUpdate(
+        {},
+        { $push: { exercise: _id } },
+        { new: true }
+      )
     );
     await ((dbWorkout) => {
       res.json(dbWorkout);
     }).catch((err) => {
       res.json(err);
+    });
+  });
+
+  // createWorkout(data)
+  app.post("/api/workouts", async ({ body }, res) => {
+    db.Workout.create(body).then(({ _id }) =>
+      db.Workout.findOneAndUpdate(
+        {},
+        { $push: { Workout: _id } },
+        { new: true }
+      )
+    );
+    await ((dbWorkout) => {
+      res.json(dbWorkout);
+    }).catch((err) => {
+      res.json(err);
+    });
+  });
+
+  // getWorkoutInRange()
+  app.get("/api/workouts/range", async (req, res) => {
+    db.Workout.find({}).sort({ _id: -1 });
+    await ((dbWorkout) => {
+      res.json(dbWorkout);
+    }).catch((err) => {
+      res.status(404).json(err);
     });
   });
 };
