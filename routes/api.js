76f6f0bffd7f285f6119f466969e22b81bd9accb
Kay0s <hamilton.kristina@gmail.com>
2021-02-04 14:29:54 -0600

    added createWorkout() route on routes/api.js

--- routes/api.js
+++ routes/api.js
@@ -2,14 +2,24 @@ const db = require("../models");
 
 module.exports = (app) => {
   // getLastWorkout()
-  app.get("/api/workouts", (req, res) => {
-    db.Workout.find({})
-      .sort({ _id: -1 })
-      .then((dbWorkout) => {
-        res.json(dbWorkout);
-      })
-      .catch((err) => {
-        res.status(404).json(err);
-      });
+  app.get("/api/workouts", async (req, res) => {
+    db.Workout.find({}).sort({ _id: -1 });
+    await ((dbWorkout) => {
+      res.json(dbWorkout);
+    }).catch((err) => {
+      res.status(404).json(err);
+    });
+  });
+
+  // addExcercise(data)
+  app.post("/api/workouts", async ({ body }, res) => {
+    db.Workout.create(body).then(({ _id }) =>
+      db.Workout.findOneAndUpdate({}, { $push: { name: _id } }, { new: true })
+    );
+    await ((dbWorkout) => {
+      res.json(dbWorkout);
+    }).catch((err) => {
+      res.json(err);
+    });
   });
 };
