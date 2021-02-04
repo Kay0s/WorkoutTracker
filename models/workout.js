124166debe309ed5cfa527b82d236c444da6991d
Kay0s <hamilton.kristina@gmail.com>
2021-02-04 14:46:53 -0600

    added excercises [] to models/workout.js

--- models/workout.js
+++ models/workout.js
@@ -3,35 +3,40 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter the name of workout",
-  },
-  type: {
-    type: String,
-    trim: true,
-    required: "Enter the type of workout",
-  },
-  distance: {
-    type: Number,
-  },
-  weight: {
-    type: Number,
-  },
-  sets: {
-    type: Number,
-  },
-  reps: {
-    type: Number,
-  },
-  duration: {
-    type: Number,
-  },
   day: {
     type: Date,
     default: Date.now,
   },
+
+  excercises: [
+    {
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter the name of workout",
+      },
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter the type of workout",
+      },
+      distance: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      duration: {
+        type: Number,
+      },
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
